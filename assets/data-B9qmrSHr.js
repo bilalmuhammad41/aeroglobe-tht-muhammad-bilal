d61f745cd637c1587fdb64e3790eb0428a48cf4a
bilalmuhammad41 <bilalmohammad41@gmail.com>
2025-02-01 14:47:34 +0500

    Updates

--- assets/data-B9qmrSHr.js
+++
@@ -1,1 +0,0 @@
-const i=[{id:"1",chips:"Lays Salted",drink:"Pepsi",chocolate:"Cadbury"},{id:"2",chips:"Slims",drink:"7Up",chocolate:"Perk"},{id:"3",chips:"Potato Sticks",drink:"Pepsi",chocolate:"Now"},{id:"4",chips:"Slanty",drink:"Miranda",chocolate:"Cadbury"},{id:"5",chips:"Slims",drink:"Pepsi",chocolate:"Perk"}],c={combos:i};export{i as combos,c as default};
